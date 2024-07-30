0377eb1981aa317bb321716eb4c04f4c9deb357c
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-07-30 16:02:20 +0530

    added log line 1257

--- controllers/Agent.controller.js
+++ controllers/Agent.controller.js
@@ -1252,7 +1252,7 @@ class AgentController extends BaseController {
 
       // Update DailySlot to mark selected slots as unavailable
       // Fetch the current DailySlot
-      console.log(selectedSlots[0].startTime);
+      console.log('Selected slot startTime:', selectedSlots[0].startTime);
       const date = new Date(selectedSlots[0].startTime)
         .toISOString()
         .split("T")[0];
